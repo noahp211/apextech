2dce767e7f22f3b1ce5c9d43dd87c6ed7c1e4286
Mushfika <itsmushfikahossain@gmail.com>
2026-03-13 15:26:27 -0400

    Added pages

---
+++ Server/apextech/src/main/java/com/cpan228/apextech/ApextechApplication.java
@@ -0,0 +1,13 @@
+package com.cpan228.apextech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApextechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApextechApplication.class, args);
+	}
+
+}
---
+++ Server/apextech/src/test/java/com/cpan228/apextech/ApextechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpan228.apextech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApextechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
